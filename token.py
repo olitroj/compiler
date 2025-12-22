4c481f4235fc6f5bb5416efe8cd07d7981db13c0
Oliver Trojanowski <trojanowskioliver1@gmail.com>
2025-12-22 17:03:57 +0100

    Add Token and TokenType classes

---
+++ token.py
@@ -0,0 +1,49 @@
+from enum import Enum
+from typing import Any
+
+class TokenType(Enum):
+    VAR = "var"
+    ID = None
+    ASSIGN = "="
+    SEMICOLON = ";"
+    IF = "if"
+    ELSE = "else"
+    WHILE = "while"
+    DO = "do"
+    OPEN_BRACE = "("
+    CLOSE_BRACE = ")"
+    COMMA = ","
+    OPEN_CURLY = "{"
+    CLOSE_CURLY = "}"
+    LITERAL = None
+    PLUS = "+"
+    MINUS = "-"
+    INCREMENT = "++"
+    DECREMENT = "--"
+    LOGIC_AND = "&&"
+    LOGIC_OR = "||"
+    LOGIC_XOR = "^^"
+    LOGIC_NOT = "!"
+    BIT_AND = "&"
+    BIT_OR = "|"
+    BIT_XOR = "^"
+    BIT_NOT = "~"
+    SHIFT_LEFT = "<<"
+    SHIFT_RIGHT = ">>"
+    LESS_THAN = "<"
+    LESS_THAN_EQUALS = "<="
+    GREATER_THAN = ">"
+    GREATER_THAN_EQUALS = ">="
+    EQUAL = "=="
+    NOT_EQUAL = "!="
+
+class Token():
+    def __init__(self, type : TokenType, value : Any):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.type.name}({self.value})"
+    
+    def __eq__(self, other : Token):
+        return self.type == other.type and self.value == other.value
